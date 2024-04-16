f3941c3053804776706aa363364859503d24bbd3
samuraychik <vitvit0312@gmail.com>
2024-04-16 18:03:31 +0500

    не 2

--- index.js
+++ index.js
@@ -8,6 +8,40 @@ function addNewBeverage() {
     const lastBeverage = allBeverages[allBeverages.length - 1];
     newBeverage.querySelector('.beverage-count').textContent = `Напиток №${++beveragesCount}`;
     lastBeverage.insertAdjacentElement('afterend', newBeverage);
+
+    for (const radio of newBeverage.querySelectorAll("input[type=radio]")) {
+        radio.name = "milk" + beveragesCount;
+    }
+    updateDeleteButtons();
+}
+
+function updateDeleteButtons() {
+    let allBeverages = document.querySelectorAll('.beverage');
+
+    allBeverages.forEach(beverage => {
+        if (allBeverages.length > 1) {
+            if (beverage.querySelectorAll('.delete_button').length === 0) {
+                const deleteButton = document.createElement('button');
+                deleteButton.classList.add('delete_button');
+                deleteButton.textContent = 'X';
+                deleteButton.style.position = 'absolut';
+                deleteButton.style.top = '0';
+                deleteButton.style.right = '0';
+                deleteButton.style.cursor = 'pointer';
+
+                deleteButton.addEventListener('click', () => {
+                    beverage.remove();
+                    allBeverages = document.querySelectorAll('.beverage');
+                    if (allBeverages.length === 1) {
+                        allBeverages[0].querySelector('.delete_button').remove();
+                    }
+                });
+
+                beverage.appendChild(deleteButton);
+            }
+        }
+
+    });
 }
 
 addButton.addEventListener('click', addNewBeverage);
