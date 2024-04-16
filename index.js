ca8734314edd5fcaa4c7d11ab3ecd1cde1f4f26f
samuraychik <vitvit0312@gmail.com>
2024-04-16 18:20:37 +0500

    #2

--- index.js
+++ index.js
@@ -1,47 +1,43 @@
 const addButton = document.querySelector('.add-button');
 const beverage = document.querySelector('.beverage');
-let beveragesCount = 1;
+
+let beveragesCount = 0;
+addNewBeverage();
+beverage.remove();
 
 function addNewBeverage() {
     const newBeverage = beverage.cloneNode(true);
-    const allBeverages = document.querySelectorAll('.beverage');
-    const lastBeverage = allBeverages[allBeverages.length - 1];
-    newBeverage.querySelector('.beverage-count').textContent = `Напиток №${++beveragesCount}`;
-    lastBeverage.insertAdjacentElement('afterend', newBeverage);
 
-    for (const radio of newBeverage.querySelectorAll("input[type=radio]")) {
-        radio.name = "milk" + beveragesCount;
-    }
-    updateDeleteButtons();
+    newBeverage.querySelector('.beverage-count').textContent = `Напиток №${++beveragesCount}`;
+    makeRadioUnique(newBeverage);
+    appendDeleteButton(newBeverage);
+
+    const allBeverages = document.querySelectorAll('.beverage');
+    allBeverages[allBeverages.length - 1].after(newBeverage);
 }
 
-function updateDeleteButtons() {
-    let allBeverages = document.querySelectorAll('.beverage');
+function makeRadioUnique(beverage) {
+    for (const radio of beverage.querySelectorAll('input[type=radio]')) {
+        radio.name = `milk${beveragesCount}`;
+    }
+}
+function appendDeleteButton(beverage) {
+    const deleteButton = document.createElement('button');
+    deleteButton.classList.add('delete_button');
+    deleteButton.textContent = 'X';
+    deleteButton.style.position = 'absolut';
+    deleteButton.style.top = '0';
+    deleteButton.style.right = '0';
+    deleteButton.style.cursor = 'pointer';
 
-    allBeverages.forEach(beverage => {
-        if (allBeverages.length > 1) {
-            if (beverage.querySelectorAll('.delete_button').length === 0) {
-                const deleteButton = document.createElement('button');
-                deleteButton.classList.add('delete_button');
-                deleteButton.textContent = 'X';
-                deleteButton.style.position = 'absolut';
-                deleteButton.style.top = '0';
-                deleteButton.style.right = '0';
-                deleteButton.style.cursor = 'pointer';
-
-                deleteButton.addEventListener('click', () => {
-                    beverage.remove();
-                    allBeverages = document.querySelectorAll('.beverage');
-                    if (allBeverages.length === 1) {
-                        allBeverages[0].querySelector('.delete_button').remove();
-                    }
-                });
-
-                beverage.appendChild(deleteButton);
-            }
+    deleteButton.addEventListener('click', () => {
+        if (beveragesCount > 1) {
+            beverage.remove();
+            beveragesCount--;
         }
-
     });
+
+    beverage.appendChild(deleteButton);
 }
 
 addButton.addEventListener('click', addNewBeverage);
